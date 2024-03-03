aaaee29a2eab4db162598de8b4c43cc0f474785b
AgustinSegui <agussegui24@hotmail.es>
2024-03-03 14:12:11 -0300

    buscador de bebidas

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import {Container} from 'react-bootstrap'
+import Formulario from './components/Formulario'
+import { CategoriasProvider } from './context/CategoriasProvider'
+import { BebidasProvider } from './context/BebidasProvider'
+
+
+const App = () => {
+  return (
+    <CategoriasProvider>
+      <BebidasProvider>
+        <header className="py-5">
+          <h1>Buscador de Bebidas</h1>
+        </header>
+    
+        <Container className='mt-5'>
+          <Formulario/>
+        </Container>
+      </BebidasProvider>
+    </CategoriasProvider>
+  )
+}
+
+export default App
